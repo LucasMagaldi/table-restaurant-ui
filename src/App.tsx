ba47440a28e28dcee5ecd70199579a27fdb9be4d
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-10-13 23:53:59 -0300

    set up pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
 export function App() {
-  return <h1>app</h1>
+  return (
+    <RouterProvider router={router}/>
+  )
 }
