73f0c38f8d0c2daaa2125f1f62a598eb3007e7f2
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-10-14 00:00:16 -0300

    set up layouts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+import { AppLayout } from '@/pages/_layout/app-layout'
+import { AuthLayout } from '@/pages/_layout/auth-layout'
 import { Dashboard } from '@/pages/app/dashboard'
 import { SignIn } from '@/pages/auth/sign-in'
 import { createBrowserRouter } from 'react-router-dom'
@@ -5,10 +7,22 @@ import { createBrowserRouter } from 'react-router-dom'
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Dashboard />
+        element: <AppLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Dashboard />
+            }
+        ]
     }, 
     {
         path: '/sign-in',
-        element: <SignIn />
+        element: <AuthLayout />,
+        children: [
+            {
+                path: '/sign-in',
+                element: <SignIn />
+            }
+        ]
     }
 ])
