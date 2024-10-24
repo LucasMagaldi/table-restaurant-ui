21d7027d22d54576794719f59fca5804039ea4fa
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-10-23 22:39:37 -0300

    feat: sign up page

---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,83 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { z } from "zod";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const signUpFormSchema = z.object({
+    restaurant: z.string(),
+    name: z.string(),
+    email: z.string().email(),
+    phone: z.string()
+})
+
+type SignUpForm = z.infer<typeof signUpFormSchema>
+
+export function SignUp() {
+    const navigate = useNavigate()
+
+    const { 
+        register,
+        handleSubmit,
+        formState: {
+            isSubmitting
+        }
+    } = useForm<SignUpForm>()
+
+    async function handleSignUp(data: SignUpForm) {
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+
+            toast.success('Registering restaurant successfully', {
+                action: {
+                    label: 'Login',
+                    onClick: () => navigate('/sign-in')
+                }
+            })
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (error) {
+            toast.error('Error while registering restaurant')
+        }
+    }
+
+    return (
+        <div className="p-8">
+            <div className="flex flex-col w-[350px] justify-center gap-6">
+                <div className="flex flex-col text-center">
+                    <h1 className="font-semibold text-2xl tracking-tighter">
+                        Create free account
+                    </h1>
+                    <p className="text-sm text-muted-foreground">
+                        Be part of the best platform to manage sales
+                    </p>
+                </div>
+                <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="space-y-2">
+                        <Label htmlFor="restaurant">Restaurant name</Label>
+                        <Input id="restaurant" type="text" {...register('restaurant')}/>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Label htmlFor="name">Your Name</Label>
+                        <Input id="name" type="text" {...register('name')}/>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Label htmlFor="email">Your email</Label>
+                        <Input id="email" type="email" {...register('email')}/>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Label htmlFor="phone">Your phone</Label>
+                        <Input id="phone" type="tel" {...register('phone')}/>
+                    </div>
+                    <Button className="w-full" disabled={isSubmitting} type="submit">Sign up</Button>
+                </form>
+            </div>
+        </div>
+    )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { AppLayout } from '@/pages/_layout/app-layout'
 import { AuthLayout } from '@/pages/_layout/auth-layout'
 import { Dashboard } from '@/pages/app/dashboard'
 import { SignIn } from '@/pages/auth/sign-in'
+import { SignUp } from '@/pages/auth/sign-up'
 import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
@@ -16,13 +17,17 @@ export const router = createBrowserRouter([
         ]
     }, 
     {
-        path: '/sign-in',
+        path: '/',
         element: <AuthLayout />,
         children: [
             {
                 path: '/sign-in',
                 element: <SignIn />
+            },
+            {
+                path: '/sign-up',
+                element: <SignUp />
             }
         ]
-    }
+    },
 ])
