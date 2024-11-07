26dce0fd86b5166087e2251319010c3ecd99ef40
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-11-07 20:56:47 -0300

    feat: add skeleton

--- src/components/account-menu/index.tsx
+++ src/components/account-menu/index.tsx
@@ -5,15 +5,16 @@ import { Button } from '../ui/button'
 import { useQuery } from '@tanstack/react-query'
 import { getUserProfile } from '@/hooks/api-user'
 import { getRestaurantInfo } from '@/hooks/api-restaurant'
+import { Skeleton } from '../ui/skeleton'
 
 
 export function AccountMenu() {
-    const { data: userProfile } = useQuery({
+    const { data: userProfile, isLoading: isGetUserProfileLoading } = useQuery({
         queryKey: ['user-profile'],
         queryFn: getUserProfile
     })
 
-    const { data: restaurantInfo } = useQuery({
+    const { data: restaurantInfo, isLoading: isGetRestaurantInfoLoading } = useQuery({
         queryKey: ['restaurant-info'],
         queryFn: getRestaurantInfo
     })
@@ -25,20 +26,36 @@ export function AccountMenu() {
                     variant="outline"
                     className='flex items-center gap-2'
                 >
-                    {restaurantInfo?.name}
-                    <ChevronDown className="h-4 w-4" />
+                    {isGetRestaurantInfoLoading ? (
+                        <Skeleton className="h-4 w-[150px]" />
+                    ) : (
+                        <>
+                            {restaurantInfo?.name}
+                            <ChevronDown className="h-4 w-4" />
+                        </>
+                    )}
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align='end'>
                 <DropdownMenuLabel
                     className='flex flex-col'
                 >
-                    <span>{userProfile?.name}</span>
-                    <span
-                        className='text-muted-foreground text-xs font-normal'
-                    >
-                        {userProfile?.email}
-                    </span>
+                    {isGetUserProfileLoading ? (
+                        <div className='flex flex-col gap-2'>
+                            <Skeleton className="h-4 w-[250px]" />
+                            <Skeleton className="h-4 w-[200px]" />
+                        </div>
+                    ) : (
+                        <>
+                            <span>{userProfile?.name}</span>
+                            <span
+                                className='text-muted-foreground text-xs font-normal'
+                            >
+                                {userProfile?.email}
+                            </span>
+                        </>
+                    )}
+                   
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem className='cursor-pointer'>
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,3 +4,10 @@ export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
+
+if(import.meta.env.VITE_API_REQUEST_DELAY) {
+    api.interceptors.request.use(async (config) => {
+        await new Promise((resolve) =>setTimeout(resolve, 2000))
+        return config
+    })
+}
