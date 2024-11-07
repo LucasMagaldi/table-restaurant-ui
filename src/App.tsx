d088fbf58790c76efec7c97419acb25c26e954c0
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-11-07 13:38:10 -0300

    feat: api connection

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { Toaster } from "sonner";
 
 import { router } from "./routes";
 import { ThemeProvider } from "./context/theme";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark">
       <Toaster richColors/>
-      <RouterProvider router={router}/>
+      <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}/>
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
