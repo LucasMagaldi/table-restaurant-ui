178648442dbfb6464666fb5044227fcdb8d29c86
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-11-07 22:17:40 -0300

    feat: account menu - update profile info

--- src/components/account-menu/edit-profile-modal.tsx
+++ src/components/account-menu/edit-profile-modal.tsx
@@ -1,10 +1,49 @@
+import { z } from "zod";
 import { Button } from "../ui/button";
 import { DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Textarea } from "../ui/textarea";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { useMutation } from "@tanstack/react-query";
+import { updateUserProfile } from "@/hooks/api-user";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const editProfileSchema = z.object({
+    name: z.string(),
+    description: z.string()
+})
+
+type EditProfileForm = z.infer<typeof editProfileSchema>
 
 export function EditProfileModal() {
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting }
+    } = useForm<EditProfileForm>({
+
+    })
+
+    const { mutateAsync: updateUserProfileFn } = useMutation({
+        mutationFn: updateUserProfile
+    })
+
+    async function handleUpdateProfile(data: EditProfileForm) {
+        try {
+            await updateUserProfileFn({
+                name: data.name,
+                description: data.description
+            })
+
+            toast.success('profile updated')
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (error) {
+            toast.error('An error happen during the update process')
+        }
+    }
+
     return (
         <DialogContent>
             <DialogHeader>
@@ -13,24 +52,25 @@ export function EditProfileModal() {
                     Update your store information visible to the customer
                 </DialogDescription>
             </DialogHeader>
-            <form className="mt-4 flex flex-col gap-6">
+            <form  onSubmit={handleSubmit(handleUpdateProfile)} className="mt-4 flex flex-col gap-6">
                 <div className="flex items-center justify-end gap-6">
                     <Label>Name</Label>
-                    <Input className="w-[350px]"/>
+                    <Input {...register('name')} className="w-[350px]"/>
                 </div>
                 <div className="flex items-center justify-end gap-6">
                     <Label>Description</Label>
-                    <Textarea className="w-[350px]"/>
+                    <Textarea {...register('description')} className="w-[350px]"/>
                 </div>
                 <DialogFooter className="flex gap-4 justify-end">
                     <DialogClose>
                         <Button 
-                        variant='ghost'>
+                        variant='ghost'
+                        disabled={!!isSubmitting}>
                             Cancel
                         </Button>                        
                     </DialogClose>
 
-                    <Button variant='default' type="submit">Save</Button>
+                    <Button variant='default' type="submit" disabled={!!isSubmitting}>Save</Button>
                 </DialogFooter>
             </form>
         </DialogContent>
--- src/hooks/api-user.ts
+++ src/hooks/api-user.ts
@@ -10,9 +10,18 @@ interface GetProfileResponse {
     updatedAt: Date | null
 }
 
+interface UpdateProfileBody {
+    name: string
+    description: string | null
+}
+
 export async function getUserProfile() {
     const respose = await api
         .get<GetProfileResponse>('/me')
     
     return respose.data
+}
+
+export async function updateUserProfile({ name, description }: UpdateProfileBody) {
+    await api.put('/profile', { name, description })
 }
